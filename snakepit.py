0eb7b1842ebdbe9c0cf7872362ec53ea0a0fed1f
Valentin Haenel <valentin@haenel.co>
2015-09-08 14:24:24 +0200

    add some more documentation

--- snakepit.py
+++ snakepit.py
@@ -18,9 +18,18 @@ from jinja2 import Template
 
 
 def add_conda_dist_flavour_prefix(loaded_yaml):
+    """ Add an first letter uppercase version of the conda_dist_flavour.
+
+    The files from Continuum at:
+
+        http://repo.continuum.io/miniconda/
+
+    all start with an uppercase letter. Hence we need an uppercase version in
+    our dictionary so that we can use it in the template.
+
+    """
     x = loaded_yaml['conad_dist_flavour']
     loaded_yaml['conad_dist_flavour_urlprefix'] = x[0].upper() + x[1:]
-
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
