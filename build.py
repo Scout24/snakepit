9880a8290e710e0bbc1644f78b7d0975c61cc79f
Valentin Haenel <valentin@haenel.co>
2015-09-10 17:41:29 +0200

    just use the commit count as version number

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ use_plugin('python.cram')
 
 name = "snakepit"
 default_task = "publish"
-version = '0.0.2.dev' + VCSRevision().get_git_revision_count()
+version = VCSRevision().get_git_revision_count()
 summary = "Package Python software as an RPM including all dependencies " \
           "(even the interpreter)."
 
